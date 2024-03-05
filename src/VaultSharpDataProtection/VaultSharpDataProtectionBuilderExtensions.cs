94df233fffba283db707cd704325765813b6e9b0
Paul Hatcherian <1835615+PaulHatch@users.noreply.github.com>
2024-03-05 23:08:38 +0100

    Target .NET 8

--- src/VaultSharpDataProtection/VaultSharpDataProtectionBuilderExtensions.cs
+++ src/VaultSharpDataProtection/VaultSharpDataProtectionBuilderExtensions.cs
@@ -1,147 +1,131 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.Extensions.DependencyInjection;
 using VaultSharpDataProtection;
 using VaultSharp;
 using VaultSharp.V1.AuthMethods.Token;
 
-namespace Microsoft.AspNetCore.DataProtection
+// ReSharper disable once CheckNamespace
+namespace Microsoft.AspNetCore.DataProtection;
+
+/// <summary>
+/// Provides registration methods for VaultSharp data protection key repository.
+/// </summary>
+public static class VaultSharpDataProtectionBuilderExtensions
 {
+    const string _defaultPath = "data-protection-keys";
+    const string _defaultMountPoint = "kv";
+
     /// <summary>
-    /// Provides registration methods for VaultSharp data protection key repository.
+    /// Configures the data protection system to persist keys to Vault
+    /// using the default path and mount point.
     /// </summary>
-    public static class VaultSharpDataProtectionBuilderExtensions
+    /// <param name="builder">The builder instance to modify.</param>
+    /// <param name="vaultClient">The client to use.</param>
+    /// <returns>
+    /// A reference to the <see cref="IDataProtectionBuilder" />.
+    /// </returns>
+    public static IDataProtectionBuilder PersistKeysToVault(
+        this IDataProtectionBuilder builder,
+        IVaultClient vaultClient) => builder.PersistKeysToVault(vaultClient, null, null);
+
+    /// <summary>
+    /// Configures the data protection system to persist keys to Vault.
+    /// </summary>
+    /// <param name="builder">The builder instance to modify.</param>
+    /// <param name="vaultClient">The client to use.</param>
+    /// <param name="path">The path to store keys to.</param>
+    /// <param name="mountPoint">The Vault key/value mount point.</param>
+    /// <returns>
+    /// A reference to the <see cref="IDataProtectionBuilder" />.
+    /// </returns>
+    public static IDataProtectionBuilder PersistKeysToVault(
+        this IDataProtectionBuilder builder,
+        IVaultClient vaultClient,
+        string path,
+        string mountPoint)
     {
-        const string _defaultPath = "data-protection-keys";
-        const string _defaultMountPoint = "kv";
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(vaultClient);
 
-        /// <summary>
-        /// Configures the data protection system to persist keys to Vault
-        /// using the default path and mount point.
-        /// </summary>
-        /// <param name="builder">The builder instance to modify.</param>
-        /// <param name="vaultClient">The client to use.</param>
-        /// <returns>
-        /// A reference to the <see cref="IDataProtectionBuilder" />.
-        /// </returns>
-        public static IDataProtectionBuilder PersistKeysToVault(
-            this IDataProtectionBuilder builder,
-            IVaultClient vaultClient) => builder.PersistKeysToVault(vaultClient, null, null);
-
-        /// <summary>
-        /// Configures the data protection system to persist keys to Vault.
-        /// </summary>
-        /// <param name="builder">The builder instance to modify.</param>
-        /// <param name="vaultClient">The client to use.</param>
-        /// <param name="path">The path to store keys to.</param>
-        /// <param name="mountPoint">The Vault key/value mount point.</param>
-        /// <returns>
-        /// A reference to the <see cref="IDataProtectionBuilder" />.
-        /// </returns>
-        public static IDataProtectionBuilder PersistKeysToVault(
-            this IDataProtectionBuilder builder,
-            IVaultClient vaultClient,
-            string path,
-            string mountPoint)
+        builder.Services.Configure<KeyManagementOptions>(o =>
         {
-            if (builder == null)
-            {
-                throw new ArgumentNullException(nameof(builder));
-            }
+            o.XmlRepository = new VaultSharpXmlRepository(vaultClient, path ?? _defaultPath, mountPoint ?? _defaultMountPoint);
+        });
 
-            if (vaultClient == null)
-            {
-                throw new ArgumentNullException(nameof(vaultClient));
-            }
-
-            builder.Services.Configure<KeyManagementOptions>(o =>
-            {
-                o.XmlRepository = new VaultSharpXmlRepository(vaultClient, path ?? _defaultPath, mountPoint ?? _defaultMountPoint);
-            });
-
-            return builder;
-        }
-
-        /// <summary>
-        /// Configures the data protection system to persist keys to Vault
-        /// using the default path and mount point.
-        /// </summary>
-        /// <param name="builder">The builder instance to modify.</param>
-        /// <param name="clientBuilder">The client builder.</param>
-        /// <returns>
-        /// A reference to the <see cref="IDataProtectionBuilder" />.
-        /// </returns>
-        public static IDataProtectionBuilder PersistKeysToVault(
-            this IDataProtectionBuilder builder,
-            Func<IVaultClient> clientBuilder) => builder.PersistKeysToVault(clientBuilder, null, null);
-
-
-        /// <summary>
-        /// Configures the data protection system to persist keys to Vault.
-        /// </summary>
-        /// <param name="builder">The builder instance to modify.</param>
-        /// <param name="clientBuilder">The client builder.</param>
-        /// <param name="path">The path to store keys to.</param>
-        /// <param name="mountPoint">The Vault key/value mount point.</param>
-        /// <returns>
-        /// A reference to the <see cref="IDataProtectionBuilder" />.
-        /// </returns>
-        public static IDataProtectionBuilder PersistKeysToVault(
-            this IDataProtectionBuilder builder,
-            Func<IVaultClient> clientBuilder,
-            string path,
-            string mountPoint)
-        {
-            if (builder == null)
-            {
-                throw new ArgumentNullException(nameof(builder));
-            }
-
-            if (clientBuilder == null)
-            {
-                throw new ArgumentNullException(nameof(clientBuilder));
-            }
-
-            return PersistKeysToVault(builder, clientBuilder(), path, mountPoint);
-        }
-
-        /// <summary>
-        /// Configures the data protection system to persist keys to Vault.
-        /// </summary>
-        /// <param name="builder">The builder instance to modify.</param>
-        /// <param name="vaultUri">The Vault URI.</param>
-        /// <param name="token">The Vault access token.</param>
-        /// <param name="path">The path to store keys to.</param>
-        /// <param name="mountPoint">The Vault key/value mount point.</param>
-        /// <returns>
-        /// A reference to the <see cref="IDataProtectionBuilder" />.
-        /// </returns>
-        public static IDataProtectionBuilder PersistKeysToVault(
-            this IDataProtectionBuilder builder,
-            Uri vaultUri,
-            string token,
-            string path,
-            string mountPoint)
-        {
-            var client = new VaultClient(new VaultClientSettings(vaultUri.ToString(), new TokenAuthMethodInfo(token)));
-            return builder.PersistKeysToVault(client, path, mountPoint);
-        }
-
-        /// <summary>
-        /// Configures the data protection system to persist keys to Vault
-        /// using the default path and mount point.
-        /// </summary>
-        /// <param name="builder">The builder instance to modify.</param>
-        /// <param name="vaultUri">The Vault URI.</param>
-        /// <param name="token">The Vault access token.</param>
-        /// <returns>
-        /// A reference to the <see cref="IDataProtectionBuilder" />.
-        /// </returns>
-        public static IDataProtectionBuilder PersistKeysToVault(
-            this IDataProtectionBuilder builder,
-            Uri vaultUri,
-            string token) => builder.PersistKeysToVault(vaultUri, token, null, null);
+        return builder;
     }
+
+    /// <summary>
+    /// Configures the data protection system to persist keys to Vault
+    /// using the default path and mount point.
+    /// </summary>
+    /// <param name="builder">The builder instance to modify.</param>
+    /// <param name="clientBuilder">The client builder.</param>
+    /// <returns>
+    /// A reference to the <see cref="IDataProtectionBuilder" />.
+    /// </returns>
+    public static IDataProtectionBuilder PersistKeysToVault(
+        this IDataProtectionBuilder builder,
+        Func<IVaultClient> clientBuilder) => builder.PersistKeysToVault(clientBuilder, null, null);
+
+
+    /// <summary>
+    /// Configures the data protection system to persist keys to Vault.
+    /// </summary>
+    /// <param name="builder">The builder instance to modify.</param>
+    /// <param name="clientBuilder">The client builder.</param>
+    /// <param name="path">The path to store keys to.</param>
+    /// <param name="mountPoint">The Vault key/value mount point.</param>
+    /// <returns>
+    /// A reference to the <see cref="IDataProtectionBuilder" />.
+    /// </returns>
+    public static IDataProtectionBuilder PersistKeysToVault(
+        this IDataProtectionBuilder builder,
+        Func<IVaultClient> clientBuilder,
+        string path,
+        string mountPoint)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(clientBuilder);
+
+        return PersistKeysToVault(builder, clientBuilder(), path, mountPoint);
+    }
+
+    /// <summary>
+    /// Configures the data protection system to persist keys to Vault.
+    /// </summary>
+    /// <param name="builder">The builder instance to modify.</param>
+    /// <param name="vaultUri">The Vault URI.</param>
+    /// <param name="token">The Vault access token.</param>
+    /// <param name="path">The path to store keys to.</param>
+    /// <param name="mountPoint">The Vault key/value mount point.</param>
+    /// <returns>
+    /// A reference to the <see cref="IDataProtectionBuilder" />.
+    /// </returns>
+    public static IDataProtectionBuilder PersistKeysToVault(
+        this IDataProtectionBuilder builder,
+        Uri vaultUri,
+        string token,
+        string path,
+        string mountPoint)
+    {
+        var client = new VaultClient(new VaultClientSettings(vaultUri.ToString(), new TokenAuthMethodInfo(token)));
+        return builder.PersistKeysToVault(client, path, mountPoint);
+    }
+
+    /// <summary>
+    /// Configures the data protection system to persist keys to Vault
+    /// using the default path and mount point.
+    /// </summary>
+    /// <param name="builder">The builder instance to modify.</param>
+    /// <param name="vaultUri">The Vault URI.</param>
+    /// <param name="token">The Vault access token.</param>
+    /// <returns>
+    /// A reference to the <see cref="IDataProtectionBuilder" />.
+    /// </returns>
+    public static IDataProtectionBuilder PersistKeysToVault(
+        this IDataProtectionBuilder builder,
+        Uri vaultUri,
+        string token) => builder.PersistKeysToVault(vaultUri, token, null, null);
 }
--- src/VaultSharpDataProtection/VaultSharpXmlRepository.cs
+++ src/VaultSharpDataProtection/VaultSharpXmlRepository.cs
@@ -8,95 +8,94 @@ using Microsoft.AspNetCore.DataProtection.Repositories;
 using VaultSharp;
 using VaultSharp.Core;
 
-namespace VaultSharpDataProtection
+namespace VaultSharpDataProtection;
+
+/// <summary>
+/// Provides an <see cref="IXmlRepository"/> implementation for storing
+/// data protection keys in Vault using the VaultSharp client.
+/// </summary>
+/// <seealso cref="Microsoft.AspNetCore.DataProtection.Repositories.IXmlRepository" />
+public class VaultSharpXmlRepository : IXmlRepository
 {
+    private static readonly TaskFactory _factory =
+        new TaskFactory(
+            CancellationToken.None,
+            TaskCreationOptions.None,
+            TaskContinuationOptions.None,
+            TaskScheduler.Default);
+
+    private readonly IVaultClient _vault;
+    private readonly string _path;
+    private readonly string _mountPoint;
+
     /// <summary>
-    /// Provides an <see cref="IXmlRepository"/> implementation for storing
-    /// data protection keys in Vault using the VaultSharp client.
+    /// Initializes a new instance of the <see cref="VaultSharpXmlRepository"/> class.
     /// </summary>
-    /// <seealso cref="Microsoft.AspNetCore.DataProtection.Repositories.IXmlRepository" />
-    public class VaultSharpXmlRepository : IXmlRepository
+    /// <param name="vault">The vault client to use.</param>
+    /// <param name="path">The path store keys under.</param>
+    /// <param name="mountPoint">The Vault key/value mount point.</param>
+    public VaultSharpXmlRepository(IVaultClient vault, string path, string mountPoint)
     {
-        private static readonly TaskFactory _factory =
-            new TaskFactory(
-                CancellationToken.None,
-                TaskCreationOptions.None,
-                TaskContinuationOptions.None,
-                TaskScheduler.Default);
+        _vault = vault;
+        _path = path;
+        _mountPoint = mountPoint;
+    }
 
-        private readonly IVaultClient _vault;
-        private readonly string _path;
-        private readonly string _mountPoint;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="VaultSharpXmlRepository"/> class.
-        /// </summary>
-        /// <param name="vault">The vault client to use.</param>
-        /// <param name="path">The path store keys under.</param>
-        /// <param name="mountPoint">The Vault key/value mount point.</param>
-        public VaultSharpXmlRepository(IVaultClient vault, string path, string mountPoint)
+    /// <inheritdoc/>
+    public IReadOnlyCollection<XElement> GetAllElements()
+    {
+        try
         {
-            _vault = vault;
-            _path = path;
-            _mountPoint = mountPoint;
+            var response = RunSync(() => _vault.V1.Secrets.KeyValue.V2.ReadSecretAsync(_path, null, _mountPoint));
+            return response.Data.Data.Values.Select(e => XElement.Parse((string)e))
+                .ToList()
+                .AsReadOnly();
         }
-
-
-        /// <inheritdoc/>
-        public IReadOnlyCollection<XElement> GetAllElements()
+        catch (VaultApiException e) when (e.StatusCode == 404)
         {
+            return Array.Empty<XElement>();
+        }
+    }
+
+    /// <inheritdoc/>
+    public void StoreElement(XElement element, string friendlyName)
+    {
+        RunSync(async () =>
+        {
+            int? version;
+            IDictionary<string, object> value;
             try
             {
-                var response = RunSync(() => _vault.V1.Secrets.KeyValue.V2.ReadSecretAsync(_path, null, _mountPoint));
-                return response.Data.Data.Values.Select(e => XElement.Parse((string)e))
-                    .ToList()
-                    .AsReadOnly();
+                var response = await _vault.V1.Secrets.KeyValue.V2.ReadSecretAsync(_path, null, _mountPoint);
+                version = response.Data.Metadata.Version;
+                value = response.Data.Data;
             }
             catch (VaultApiException e) when (e.StatusCode == 404)
             {
-                return Array.Empty<XElement>();
+                version = null;
+                value = new Dictionary<string, object>();
             }
-        }
 
-        /// <inheritdoc/>
-        public void StoreElement(XElement element, string friendlyName)
-        {
-            RunSync(async () =>
-            {
-                int? version;
-                IDictionary<string, object> value;
-                try
-                {
-                    var response = await _vault.V1.Secrets.KeyValue.V2.ReadSecretAsync(_path, null, _mountPoint);
-                    version = response.Data.Metadata.Version;
-                    value = response.Data.Data;
-                }
-                catch (VaultApiException e) when (e.StatusCode == 404)
-                {
-                    version = null;
-                    value = new Dictionary<string, object>();
-                }
+            value[friendlyName] = element.ToString(SaveOptions.DisableFormatting);
 
-                value[friendlyName] = element.ToString(SaveOptions.DisableFormatting);
+            await _vault.V1.Secrets.KeyValue.V2.WriteSecretAsync(_path, value, version, _mountPoint);
+        });
+    }
 
-                await _vault.V1.Secrets.KeyValue.V2.WriteSecretAsync(_path, value, version, _mountPoint);
-            });
-        }
+    private static T RunSync<T>(Func<Task<T>> method)
+    {
+        return _factory.StartNew(method)
+            .Unwrap()
+            .GetAwaiter()
+            .GetResult();
+    }
 
-        private static T RunSync<T>(Func<Task<T>> method)
-        {
-            return _factory.StartNew(method)
-                .Unwrap()
-                .GetAwaiter()
-                .GetResult();
-        }
-
-        private static void RunSync(Func<Task> method)
-        {
-            _factory.StartNew(method)
-                .Unwrap()
-                .GetAwaiter()
-                .GetResult();
-        }
+    private static void RunSync(Func<Task> method)
+    {
+        _factory.StartNew(method)
+            .Unwrap()
+            .GetAwaiter()
+            .GetResult();
     }
 }
